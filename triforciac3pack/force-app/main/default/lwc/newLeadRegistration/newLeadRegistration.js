0c6f245f47e364d723051501a1fe35fe1eff83d4
Hüseyin Topal <94078684+huseyintopal@users.noreply.github.com>
2023-02-24 21:33:59 +0100

    Course Class Created

--- triforciac3pack/force-app/main/default/lwc/newLeadRegistration/newLeadRegistration.js
+++ triforciac3pack/force-app/main/default/lwc/newLeadRegistration/newLeadRegistration.js
@@ -1,9 +1,81 @@
 import { LightningElement,api } from 'lwc';
-import NAME_FIELD from '@salesforce/schema/Contact.Name';
+import NAME from '@salesforce/schema/Lead.Name';
+import EMAIL from '@salesforce/schema/Lead.Email';
+import PHONE from '@salesforce/schema/Lead.Phone';
+import ADDRESS from '@salesforce/schema/Lead.Address';
+import INFO from '@salesforce/schema/Lead.Info_Session_Date_Time__c';
+import COMPANY from '@salesforce/schema/Lead.Company';
+import INTERESTED from '@salesforce/schema/Lead.Interested_Path__c';
+
+import retrieve from '@salesforce/apex/UIcourseController.retrieve';
+
+
+
+
+
 export default class NewLeadRegistration extends LightningElement {
+
  // Expose a field to make it available in the template
- nameField = NAME_FIELD;
+ fields = [
+NAME,
+EMAIL,
+PHONE,
+ADDRESS,
+INFO,
+COMPANY,
+INTERESTED
+ ];
+ name=NAME;
+ email=EMAIL;
+ phone=PHONE;
+ address=ADDRESS;
+ info=INFO;
+ company=COMPANY;
+ interested=INTERESTED;
+
+ isLeadSent=false;
+
+ value = 'inProgress';
+
+    get options() {
+        return [
+            { label: 'New', value: 'new' },
+            { label: 'In Progress', value: 'inProgress' },
+            { label: 'Finished', value: 'finished' },
+        ];
+    }
+
+    handleChange(event) {
+        this.value = event.detail.value;
+    }
+
  // Flexipage provides recordId and objectApiName
  @api recordId;
- @api objectApiName;
+ @api objectApiName='Lead';
+
+clickhandler(){
+    this.isLeadSent=true;
+}
+
+setTimeout(() => {
+    this.isLeadSent=true;
+    
+}, 3000);
+
+connectedCallback(){
+    retrieve()
+    .then(multicourse => {
+        multicourse.forEach(course => {
+            this.options=[
+                ...this.options,
+                {
+                    label: course.name,
+                    value: course.Id
+                }
+            ];
+        }
+    })
+}
+
+
 }
