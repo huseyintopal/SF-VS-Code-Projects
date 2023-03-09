4c37b50d0882a38f241e831b251c5cda5d52ec32
Hüseyin Topal <94078684+huseyintopal@users.noreply.github.com>
2023-03-09 21:45:37 +0100

    calendly appoinment created

--- triforciac3pack/force-app/main/default/lwc/calendlyLWC/calendlyLWC.js
+++ triforciac3pack/force-app/main/default/lwc/calendlyLWC/calendlyLWC.js
@@ -1,3 +1,31 @@
-import { LightningElement } from 'lwc';
+import { api, LightningElement } from 'lwc';
 
-export default class CalendlyLWC extends LightningElement {}
+export default class CalendlyLWC extends LightningElement {
+
+    @api calendlyLink;
+
+    connectedCallback(){
+        console.log('this.calendlyLink');
+        console.log(this.calendlyLink);
+
+        this.calendlyLink=this.calendlyLink.replaceAll('%2F','/').replaceAll('%3A',':');
+        console.log(this.calendlyLink);
+
+
+
+        /*this.calendlyLink=this.calendlyLink.replaceALL('%2F','/').replaceALL('%3A',':');
+        console.log(this.calendlyLink);*/
+
+
+
+        //https://calendly.com/huseyin05topal/cw-randevu-demo
+        //https%3A%2F%2Fcalendly.com%2Fhuseyin05topal%2Fcw-randevu-demo
+        //https%3A%2F%2Fcalendly.com%2Fhuseyin05topal%2Fcw-randevu-demo
+    }
+
+
+
+
+
+
+}
