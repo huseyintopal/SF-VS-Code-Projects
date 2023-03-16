be3f2bbb706c11f2de996cc9eb479bf61e9a55d5
Hüseyin Topal <94078684+huseyintopal@users.noreply.github.com>
2023-03-16 09:28:59 +0100

    token created

--- triforciac3pack/force-app/main/default/lwc/calendlyLWC/calendlyLWC.js
+++ triforciac3pack/force-app/main/default/lwc/calendlyLWC/calendlyLWC.js
@@ -28,9 +28,15 @@ export default class CalendlyLWC extends LightningElement {
         //https://calendly.com/huseyin05topal/cw-randevu-demo
         //https%3A%2F%2Fcalendly.com%2Fhuseyin05topal%2Fcw-randevu-demo
         //https%3A%2F%2Fcalendly.com%2Fhuseyin05topal%2Fcw-randevu-demo
+        //calendly Token : eyJraWQiOiIxY2UxZTEzNjE3ZGNmNzY2YjNjZWJjY2Y4ZGM1YmFmYThhNjVlNjg0MDIzZjdjMzJiZTgzNDliMjM4MDEzNWI0IiwidHlwIjoiUEFUIiwiYWxnIjoiRVMyNTYifQ.eyJpc3MiOiJodHRwczovL2F1dGguY2FsZW5kbHkuY29tIiwiaWF0IjoxNjc4ODc0NDQ2LCJqdGkiOiJiZTFkNzJlOC1kMWRlLTRhZTYtOWMzYi1iNzA4NGJkNTAwNGQiLCJ1c2VyX3V1aWQiOiJERUFHN0lHN0dRNlhOUjRUIn0.Oaq5iuXRYDJbiRmb-PlCmnieM3azwLTewmoxNRxSeKf-BwDWRUPr_00ZYVL9IUHLBUJe-uJb5zcZj53EH52ecA
+
+        //QULBUVQ2T2JBOQDSBMTJW47CZPDMUMOC
     }
-
-
+//My Apps
+//App_TestCohort3 was created;
+//client ID: QLOKiHyq8J3CmDkJ1tb-2J8u0h8ZFpjqXubYJmD6XAk
+//client screet: SfOCWg0KR7L68HPEx40FDABGT3sH4nxnD02v86Ak-Wc
+//webhook signing key : 9dPR232OB142ScLq5DWeUjQCAmv3UFiTFAbhyg-gjCU
 
 
 
